f652b19d1a8f8a2fcf72fc5c6aa195cd48c6d215
Ade Rahman <42912479+aderahman007@users.noreply.github.com>
2018-10-03 03:33:40 -0400

    Update balikkata.js

--- balikkata.js
+++ balikkata.js
@@ -1,10 +1,10 @@
-function balikKata(kata){
-  var balik = kata.split('').reverse().join('');
-  return balik;
+function katabalik(kata){
+  var balikKata = kata.split('').reverse().join('');
+  return balikKata;
 }
 
-console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-console.log(balikKata('John Doe')); // eoD nhoJ
-console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-console.log(balikKata('Super')); // repuS
+console.log(katabalik('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(katabalik('John Doe')); // eoD nhoJ
+console.log(katabalik('I am a bookworm')); // mrowkoob a ma I
+console.log(katabalik('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(katabalik('Super')); // repuS
