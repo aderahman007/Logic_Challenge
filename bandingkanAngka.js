369ed3f6b43b5ccfabd7894e5716ae7e8a5cf1be
Ade Rahman <42912479+aderahman007@users.noreply.github.com>
2018-10-03 03:36:00 -0400

    Update bandingkanAngka.js

--- bandingkanAngka.js
+++ bandingkanAngka.js
@@ -1,4 +1,4 @@
-function bandingAngka(angka1, angka2){
+function bandingangka(angka1, angka2){
 if (angka1 === angka2){
 return -1;
 }
@@ -6,8 +6,8 @@ return angka1 < angka2
 }
 
   
-console.log(bandingAngka(5, 8)); // true
-console.log(bandingAngka(5, 3)); // false
-console.log(bandingAngka(4, 4)); // -1
-console.log(bandingAngka(3, 3)); // -1
-console.log(bandingAngka(17, 2)); // false
+console.log(bandingangka(5, 8)); // true
+console.log(bandingangka(5, 3)); // false
+console.log(bandingangka(4, 4)); // -1
+console.log(bandingangka(3, 3)); // -1
+console.log(bandingangka(17, 2)); // false
